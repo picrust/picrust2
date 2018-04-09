f5e729dde2629134666cd2c53f5ca68a62db8f1f
Gavin Douglas <gavinmdouglas@gmail.com>
2018-04-09 14:01:33 -0400

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 from glob import glob
 
-
 __copyright__ = "Copyright 2018, The PICRUSt Project"
 __license__ = "GPL"
 __version__ = "2-alpha.6"
 __maintainer__ = "Gavin Douglas"
-__email__ = "gavin.douglas@dal.ca"
 
-long_description = ("PICRUSt: Phylogenetic Investigation of Communities by "
+long_description = ("Please visit the google group here if you have questions: "
+                    "https://groups.google.com/forum/#!forum/picrust-users. "
+                    "Citation: Phylogenetic Investigation of Communities by "
                     "Reconstruction of Unobserved States\n\n"
                     "Predictive functional profiling of microbial communities "
                     "using 16S rRNA marker gene sequences. Langille, M. "
@@ -19,20 +19,15 @@ long_description = ("PICRUSt: Phylogenetic Investigation of Communities by "
                     "E.; Vega Thurber, R. L.; Knight, R.; Beiko, R. G.; and "
                     "Huttenhower, C. Nature Biotechnology, 1-10. 8 2013.")
 
-
 setup(name='PICRUSt2',
       version=__version__,
       description=('PICRUSt: Phylogenetic Investigation of Communities by '
                    'Reconstruction of Unobserved States'),
-      author=__maintainer__,
-      author_email=__email__,
       maintainer=__maintainer__,
-      maintainer_email=__email__,
       url='https://github.com/picrust/picrust2/wiki',
       packages=['picrust2'],
       scripts=glob('scripts/*py'),
       install_requires=['numpy >= 1.5.1',
-                        'cogent == 1.5.3',
 			'h5py == 2.7.1',
                         'biom-format >= 2.1.4, < 2.2.0',
                         'future == 0.16'],
