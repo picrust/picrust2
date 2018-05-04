75f1120b84fd52a5bafa5550497136b9bc29f42c
Gavin Douglas <gavinmdouglas@gmail.com>
2018-05-04 10:05:15 -0400

    added new test scripts for util.py and metagenome_pipeline.py

---
+++ tests/test_metagenome_pipeline.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright 2018, The PICRUSt Project"
+__license__ = "GPL"
+__version__ = "2-alpha.8"
+
+import unittest
+from os import path
+import pandas as pd
+import biom
+from tempfile import TemporaryDirectory
+from picrust2.util import (get_picrust_project_dir, biom_to_pandas_df,
+                           three_df_index_overlap_sort)
+from picrust2.metagenome_pipeline import (run_metagenome_pipeline,
+                                          norm_by_marker_copies)
+
+# Set paths to test files.
+test_dir_path = path.join(get_picrust_project_dir(), "tests", "test_data",
+                          "metagenome_pipeline")
+
+seqtab_biom = path.join(test_dir_path, "test_input_sequence_abun.biom")
+seqtab_tsv = path.join(test_dir_path, "test_input_sequence_abun.tsv")
+func_predict = path.join(test_dir_path, "test_predicted_func.tsv")
+marker_predict = path.join(test_dir_path, "test_predicted_marker.tsv")
+
+exp_strat = path.join(test_dir_path, "metagenome_out",
+                      "pred_metagenome_strat.tsv")
+
+exp_unstrat = path.join(test_dir_path, "metagenome_out",
+                        "pred_metagenome_unstrat.tsv")
+
+exp_norm = path.join(test_dir_path, "metagenome_out", "seqtab_norm.tsv")
+
+# Read in test inputs and expected files.
+func_predict_in = pd.read_table(func_predict, sep="\t", index_col="tips")
+marker_predict_in = pd.read_table(marker_predict, sep="\t", index_col="tips")
+
+exp_strat_in = pd.read_table(exp_strat, sep="\t")
+
+exp_unstrat_in = pd.read_table(exp_unstrat, sep="\t", index_col="function")
+
+exp_norm_in = pd.read_table(exp_norm, sep="\t", index_col="sequence")
+
+class metagenome_pipeline_test(unittest.TestCase):
+
+    def test_full_pipeline_strat_tsv(self):
+        '''Test that run_metagenome_pipeline works on tsv input seqtab.'''
+
+        with TemporaryDirectory() as temp_dir:
+            strat_out, unstrat_out = run_metagenome_pipeline(input_biom=seqtab_tsv,
+                                                             function=func_predict,
+                                                             marker=marker_predict,
+                                                             out_dir=temp_dir)
+
+        # Need to reset index names since these aren't in output files.
+        strat_out.index = range(30)
+    
+        pd.testing.assert_frame_equal(strat_out, exp_strat_in)
+
+    def test_full_pipeline_unstrat_tsv(self):
+        '''Test that run_metagenome_pipeline works on tsv input seqtab.'''
+
+        with TemporaryDirectory() as temp_dir:
+            strat_out, unstrat_out = run_metagenome_pipeline(input_biom=seqtab_tsv,
+                                                             function=func_predict,
+                                                             marker=marker_predict,
+                                                             out_dir=temp_dir)
+
+        pd.testing.assert_frame_equal(unstrat_out, exp_unstrat_in)
+
+
+    def test_full_pipeline_strat_biom(self):
+        '''Test that run_metagenome_pipeline works on tsv input seqtab.'''
+
+        with TemporaryDirectory() as temp_dir:
+            strat_out, unstrat_out = run_metagenome_pipeline(input_biom=seqtab_biom,
+                                                             function=func_predict,
+                                                             marker=marker_predict,
+                                                             out_dir=temp_dir)
+        # Need to reset index names since these aren't in output files.
+        strat_out.index = range(30)
+    
+        pd.testing.assert_frame_equal(strat_out, exp_strat_in)
+
+    def test_full_pipeline_unstrat_biom(self):
+        '''Test that run_metagenome_pipeline works on tsv input seqtab.'''
+
+        with TemporaryDirectory() as temp_dir:
+            strat_out, unstrat_out = run_metagenome_pipeline(input_biom=seqtab_biom,
+                                                             function=func_predict,
+                                                             marker=marker_predict,
+                                                             out_dir=temp_dir)
+
+        pd.testing.assert_frame_equal(unstrat_out, exp_unstrat_in)
+
+    def test_norm_by_marker_copies(self):
+        '''Test that expected normalized sequence abundance table generated.'''
+
+        seqtab_in = biom_to_pandas_df(biom.load_table(seqtab_biom))
+
+        # Get output index labels in same order as expected.
+        seqtab_in = seqtab_in.reindex(exp_norm_in.index)
+
+        test_norm = norm_by_marker_copies(input_seq_counts=seqtab_in,
+                                          input_marker_num=marker_predict_in,
+                                          norm_filename=None)
+
+        # Test whether normalized table matches expected table.
+        pd.testing.assert_frame_equal(test_norm, exp_norm_in)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_util.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright 2018, The PICRUSt Project"
+__license__ = "GPL"
+__version__ = "2-alpha.8"
+
+import unittest
+from os import path
+import pandas as pd
+from tempfile import TemporaryDirectory
+from picrust2.util import (write_fasta, read_fasta, write_phylip, read_phylip,
+                           three_df_index_overlap_sort)
+
+# Inititalize 3 test pandas dataframes.
+test1 = pd.DataFrame.from_dict({ "a" : [1, 2, 3],
+                                 "b" : [10, 6, 5],
+                                 "c" : [1, 3, 4] },
+                                 orient='index')
+
+test2 = pd.DataFrame.from_dict({ "b" : [10, 7, 0],
+                                 "c" : [0, 0, 0],
+                                 "d" : [5, 4, 3] },
+                                 orient='index')
+
+test3 = pd.DataFrame.from_dict({ "e" : [0, 5, 3],
+                                 "c" : [9, 0, 7],
+                                 "b" : [1, 0, 2] },
+                                 orient='index')
+
+class util_test(unittest.TestCase):
+
+    def test_read_write_fasta(self):
+        '''Basic test that FASTA files are read and written correctly.'''
+
+        test_seqs_dict = {"seq1" : "GNATNGAC",
+                           "seq2" : "GTCGTGGC",
+                           "seq3" : "GNCTGAGATTAACC"}
+
+        # Write these sequences temp file and then read them back in again.
+        with TemporaryDirectory() as temp_dir:
+            outfile = path.join(temp_dir, "test.fna")
+
+            write_fasta(test_seqs_dict, outfile)
+
+            test_seqs_dict_in = read_fasta(outfile)
+        
+        self.assertEqual(test_seqs_dict, test_seqs_dict_in)
+
+    def test_read_write_phylip(self):
+        '''Basic test that Phylip files are read and written correctly.'''
+
+        test_seqs_dict = {"seq1" : "GNATNGAC",
+                          "seq2" : "GTCGTGGC",
+                          "seq3" : "GNCTGAGA"}
+
+        # Write these sequences temp file and then read them back in again.
+        with TemporaryDirectory() as temp_dir:
+            outfile = path.join(temp_dir, "test.phylip")
+
+            write_phylip(test_seqs_dict, outfile)
+
+            test_seqs_dict_in = read_phylip(outfile)
+        
+        self.assertEqual(test_seqs_dict, test_seqs_dict_in)
+
+    def test_three_df_index_overlap_sort(self):
+        '''Test that function to subset and sort dataframes to overlapping
+        index labels is generating expected output.'''
+
+        # Get subset of overlapped index labels ordered for each dataframe.
+        test1_out, test2_out, test3_out = three_df_index_overlap_sort(test1,
+                                                                      test2,
+                                                                      test3)
+
+        # Get expected output with different approach as well.
+        exp_out = [test1.loc[["b", "c"], :], test2.loc[["b", "c"], :],
+                   test3.loc[["b", "c"], :]]
+
+        # Check that output dataframes match the expected objects.
+        pd.testing.assert_frame_equal(test1_out, exp_out[0])
+        pd.testing.assert_frame_equal(test2_out, exp_out[1])
+        pd.testing.assert_frame_equal(test3_out, exp_out[2])
+
+    def test_three_df_index_overlap_sort_err(self):
+        '''Test that function to subset and sort dataframes to overlapping
+        index labels will give error if index label don't overlap..'''
+
+        # Define df without overlapping index labels.
+        test_no_overlap = pd.DataFrame.from_dict({ "e" : [0, 5, 3],
+                                                   "f" : [9, 0, 7],
+                                                   "g" : [1, 0, 2] },
+                                                 orient='index')
+
+        # Check that ValueError assertion raised.
+        self.assertRaises(ValueError, three_df_index_overlap_sort, test1,
+                          test2, test_no_overlap)
+
+if __name__ == '__main__':
+    unittest.main()
