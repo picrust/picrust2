04ea8e42aecab356f4cadecae8eb67a7afa0f0ec
Gavin Douglas <gavinmdouglas@gmail.com>
2018-06-11 14:40:49 +0000

    incremented version to 2.0.0-b.3

--- picrust2/wrap_hsp.py
+++ picrust2/wrap_hsp.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __copyright__ = "Copyright 2018, The PICRUSt Project"
 __license__ = "GPL"
-__version__ = "2.0.0-b.2"
+__version__ = "2.0.0-b.3"
 
 from os import path
 import pandas as pd
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from glob import glob
 
 __copyright__ = "Copyright 2018, The PICRUSt Project"
 __license__ = "GPL"
-__version__ = "2.0.0-b.2"
+__version__ = "2.0.0-b.3"
 __maintainer__ = "Gavin Douglas"
 
 long_description = ("Please visit the google group here if you have questions: "
