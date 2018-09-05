ead9dbfee85365dc5b029f16002e5e73cb6c5319
Gavin Douglas <gavinmdouglas@gmail.com>
2018-09-05 12:56:55 +0000

    bumped beta version number

--- scripts/run_minpath.py
+++ scripts/run_minpath.py
@@ -2,7 +2,7 @@
 
 __copyright__ = "Copyright 2018, The PICRUSt Project"
 __license__ = "GPL"
-__version__ = "2.0.0-b.6"
+__version__ = "2.0.0-b.7"
 
 import argparse
 from picrust2.run_minpath import run_minpath_pipeline
