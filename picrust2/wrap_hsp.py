78c38a5b098a151a19828f6a5aac5427b58ef40f
Gavin Douglas <gavinmdouglas@gmail.com>
2018-10-16 19:40:05 +0000

    removed underscores from genome names

--- picrust2/wrap_hsp.py
+++ picrust2/wrap_hsp.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 __copyright__ = "Copyright 2018, The PICRUSt Project"
 __license__ = "GPL"
-__version__ = "2.0.2-b"
+__version__ = "2.0.3-b"
 
 from os import path
 import pandas as pd
--- scripts/print_picrust2_config.py
+++ scripts/print_picrust2_config.py
@@ -2,7 +2,7 @@
 
 __copyright__ = "Copyright 2018, The PICRUSt Project"
 __license__ = "GPL"
-__version__ = "2.0.2-b"
+__version__ = "2.0.3-b"
 
 from sys import platform, version as python_version, executable
 
