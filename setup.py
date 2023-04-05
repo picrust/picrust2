1353f54d3dc94c2dc9a947b30bfbff890f198b92
Gavin Douglas <gavinmdouglas@gmail.com>
2023-04-05 13:11:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ __license__ = "GPL"
 __version__ = "2.5.2"
 __maintainer__ = "Gavin Douglas"
 
-long_description = ("Please visit the google group if you have questions: "
-                    "https://groups.google.com/forum/#!forum/picrust-users. "
-                    "Paper: Douglas et al. 2020. PICRUSt2 for prediction of "
+long_description = ("Douglas et al. 2020. PICRUSt2 for prediction of "
                     "metagenome functions. Nature Biotechnology. doi: "
                     "10.1038/s41587-020-0548-6.")
 
